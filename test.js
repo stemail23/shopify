0aa620f39a80836ea56b5418e27c5633cedc6181
Steven Barlow <stemail23@gmail.com>
2017-02-05 14:57:07 +1100

    Simple retrieve smart collections example

--- test.js
+++ test.js
@@ -3,4 +3,7 @@ const shopifyModule = require('./lib/shopify');
 
 var shopify = new shopifyModule.Shopify(config);
 
-shopify.foo();
+shopify.foo(function(err, result) {
+	if (err) { return console.error(err); }
+	console.log(result);
+});
