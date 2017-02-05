0212f2a9424de3715d65da6ccce497bb91d4acf8
Steven Barlow <stemail23@gmail.com>
2017-02-05 15:46:42 +1100

    Generic api for get all

--- test.js
+++ test.js
@@ -3,7 +3,10 @@ const shopifyModule = require('./lib/shopify');
 
 var shopify = new shopifyModule.Shopify(config);
 
-shopify.foo(function(err, result) {
+shopify.smart_collections.all(function(err, result) {
 	if (err) { return console.error(err); }
 	console.log(result);
+	// result.forEach(function(item) {
+	// 	console.log(item.id, item.handle, item.title);
+	// });
 });
